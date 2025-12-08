5a4d92632a561bea7de47954212a7a0e84726044
Isaac Davi <102384604+isaacdavii@users.noreply.github.com>
2025-12-08 15:54:46 -0300

    feat(schema): aprimora regras de validação de senha
    Novas regras aplicadas para senha:
    - Mínimo de 8 caracteres.
    - Pelo menos uma letra maiúscula e uma minúscula.
    - Pelo menos um número.
    - Pelo menos um caractere especial.

--- packages/schema/src/auth.ts
+++ packages/schema/src/auth.ts
@@ -17,7 +17,19 @@ export type Login = z.infer<typeof LoginSchema>;
 
 export const RegisterSchema = z.object({
   email: z.email("Email inválido").nonempty().trim(),
-  password: z.string().nonempty().trim().min(8).max(50),
+  password: z
+    .string()
+    .nonempty()
+    .trim()
+    .min(8)
+    .max(50)
+    .regex(/\d/, "A senha deve conter ao menos um número")
+    .regex(/[a-z]/, "A senha deve conter ao menos uma letra minúscula")
+    .regex(/[A-Z]/, "A senha deve conter ao menos uma letra maiúscula")
+    .regex(
+      /[!@#$%^&*(),.?":{}|<>]/,
+      "A senha deve conter ao menos um caractere especial"
+    ),
   username: z.string().nonempty().trim(),
 });
 
